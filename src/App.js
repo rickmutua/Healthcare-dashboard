601d152b69da0b61387be2f2ffb371e785732461
toelapiut <toelapiut7@gmail.com>
2018-02-08 08:53:16 +0300

    feat(component ) Navigation
    import navigation bar to the root app and styled it

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+import Routes from './routes/Routes';
+
+// imported naviagation bar to the root folder because its not going to change
+import Navigation from './components/Navigation';
 
 
 class App extends Component {
   render() {
     return (
       <div className="contain">
-        Patient Dashboard 
+       <Navigation/>
+       <Routes/>
       </div>
     );
   }
