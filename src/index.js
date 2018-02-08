eb0f12981e47a339309551fa0f9bf3129548b2fd
toelapiut <toelapiut7@gmail.com>
2018-02-08 11:43:59 +0300

    imported styles

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ import registerServiceWorker from './registerServiceWorker';
 // imported BrowserRouter from react-router-dom to make it possible navigation through the application
 import {BrowserRouter} from 'react-router-dom';
 
+//importing general styles
+import general from './styles/general/general.css';
+
+
 
 // This is the entry level 
 ReactDOM.render(
